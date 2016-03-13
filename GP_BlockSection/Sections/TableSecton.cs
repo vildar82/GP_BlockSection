485074497173e9a8ea59ce6b888302570428ce66
Vildar Khisyametdinov <vildar82@gmail.com>
2016-03-13 13:53:42 +0300

    Изменение таблицы блок-секций. Без шапки.

--- GP_BlockSection/Sections/TableSecton.cs
+++ GP_BlockSection/Sections/TableSecton.cs
@@ -29,103 +29,117 @@ namespace GP_BlockSection.Sections
 
             var data = _service.DataSection;
 
-            table.SetSize(13, data.SectionTypes.Count + 1);
+            table.SetSize(10, 2);
 
-            foreach (var column in table.Columns)
+            table.Columns[0].Width = 55;
+            table.Columns[1].Width = 25;
+
+            foreach (var item in table.Rows)
             {
-                column.Width = 30;
-                column.Alignment = CellAlignment.MiddleCenter;
+                item.Height = 8;
             }
 
+            //foreach (var column in table.Columns)
+            //{
+            //    column.Width = 30;
+            //    column.Alignment = CellAlignment.MiddleCenter;
+            //}
+            
             table.Columns[0].Alignment = CellAlignment.MiddleLeft;
-            table.Rows[1].Height = 15;
+            table.Columns[1].Alignment = CellAlignment.MiddleCenter;
+            //table.Rows[1].Height = 15;
 
-            table.Cells[0, 0].TextString = "Блок-секции";
+            table.Cells[0, 0].TextString = "Московская область (РНГП №713/30)";
             table.Cells[0, 0].Alignment = CellAlignment.MiddleCenter;
 
-            table.Cells[1, 0].TextString = "Наименование блок-секции";
-            table.Cells[1, 0].Alignment = CellAlignment.MiddleCenter;
-            table.Cells[2, 0].TextString = "Площадь квартир, м.кв.";
-            table.Cells[2, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[3, 0].TextString = "Площадь БКФН, м.кв.";
-            table.Cells[3, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[4, 0].TextString = "Количество секций";
-            table.Cells[4, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[5, 0].TextString = "Средняя этажность";
-            table.Cells[5, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[6, 0].TextString = "Всего площадь жилого фонда, м.кв.";
-            table.Cells[6, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[7, 0].TextString = "Всего площадь квартир, м.кв.";
-            table.Cells[7, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[8, 0].TextString = "Всего площадь БКФН, м.кв.";
+            //table.Cells[1, 0].TextString = "Наименование блок-секции";
+            //table.Cells[1, 0].Alignment = CellAlignment.MiddleCenter;
+            //table.Cells[2, 0].TextString = "Площадь квартир, м.кв.";
+            //table.Cells[2, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            //table.Cells[3, 0].TextString = "Площадь БКФН, м.кв.";
+            //table.Cells[3, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            //table.Cells[4, 0].TextString = "Количество секций";
+            //table.Cells[4, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[1, 0].TextString = "Всего площадь жилого фонда, м.кв.";
+            table.Cells[1, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[2, 0].TextString = "Всего площадь квартир, м.кв.";
+            table.Cells[2, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[3, 0].TextString = "Всего площадь БКФН, м.кв.";
+            table.Cells[3, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[4, 0].TextString = "Средняя этажность";
+            table.Cells[4, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[5, 0].TextString = "Жителей, чел";
+            table.Cells[5, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[6, 0].TextString = "ДОО, чел";
+            table.Cells[6, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[7, 0].TextString = "СОШ, чел";
+            table.Cells[7, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;            
+            table.Cells[8, 0].TextString = @"Машиноместа, м/м 420/1000"; // "\\A1;\\pxt8;Машиноместа, м/м\\P\\ptz;{\\H0.6x;420/1 000}"
             table.Cells[8, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[9, 0].TextString = "ДОО, чел";
+            table.Cells[9, 0].TextString = "Машиноместа гостевые, м/м (25%)";
             table.Cells[9, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[10, 0].TextString = "СОШ, чел";
-            table.Cells[10, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[11, 0].TextString = @"Машиноместа, м/м 420/1000"; // "\\A1;\\pxt8;Машиноместа, м/м\\P\\ptz;{\\H0.6x;420/1 000}"
-            table.Cells[11, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[12, 0].TextString = "Машиноместа гостевые, м/м (25%)";
-            table.Cells[12, 0].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
 
             var titleCells = CellRange.Create(table, 1, 0, 1, table.Columns.Count - 1);
             titleCells.Borders.Bottom.LineWeight = LineWeight.LineWeight030;
 
             // Параметры по типам секций
-            int col = 1;
-            foreach (var sectType in data.SectionTypes)
-            {
-                table.Cells[1, col].TextString = sectType.Name; //Наименование
-                table.Cells[2, col].TextString = sectType.AreaApartTotal.ToString("0.0");  //Площадь квартир
-                table.Cells[3, col].TextString = sectType.AreaBKFN.ToString("0.0");  // Площадь БКФН
-                table.Cells[4, col].TextString = sectType.Count.ToString();// Кол секций
-                col++;
-            }
-            var mCells = CellRange.Create(table, 5, 1, 5, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 6, 1, 6, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 7, 1, 7, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 8, 1, 8, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 9, 1, 9, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 10, 1, 10, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 11, 1, 11, table.Columns.Count - 1);
-            table.MergeCells(mCells);
-            mCells = CellRange.Create(table, 12, 1, 12, table.Columns.Count - 1);
-            table.MergeCells(mCells);
+            //int col = 1;
+            //foreach (var sectType in data.SectionTypes)
+            //{
+            //    table.Cells[1, col].TextString = sectType.Name; //Наименование
+            //    table.Cells[2, col].TextString = sectType.AreaApartTotal.ToString("0.0");  //Площадь квартир
+            //    table.Cells[3, col].TextString = sectType.AreaBKFN.ToString("0.0");  // Площадь БКФН
+            //    table.Cells[4, col].TextString = sectType.Count.ToString();// Кол секций
+            //    col++;
+            //}
+            //var mCells = CellRange.Create(table, 5, 1, 5, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 6, 1, 6, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 7, 1, 7, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 8, 1, 8, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 9, 1, 9, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 10, 1, 10, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 11, 1, 11, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
+            //mCells = CellRange.Create(table, 12, 1, 12, table.Columns.Count - 1);
+            //table.MergeCells(mCells);
 
             // Общие параметры по всем типам секций
 
-            // Средняя этажность
-            table.Cells[5, 1].TextString = data.AverageFloors.ToString("0.0"); 
-            table.Cells[5, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
-            table.Cells[5, 1].Borders.Top.LineWeight = LineWeight.LineWeight030;
             // Всего площадь жилого фонда
-            table.Cells[6, 1].TextString = (data.TotalAreaApart + data.TotalAreaBKFN).ToString("0.0");
-            table.Cells[6, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[1, 1].TextString = (data.TotalAreaApart + data.TotalAreaBKFN).ToString("0.0");
+            table.Cells[1, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             // ВСЕГО ПЛОЩАДЬ КВАРТИР
-            table.Cells[7, 1].TextString = data.TotalAreaApart.ToString("0.0");
-            table.Cells[7, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[2, 1].TextString = data.TotalAreaApart.ToString("0.0");
+            table.Cells[2, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             // ВСЕГО ПЛОЩАДЬ БКФН
-            table.Cells[8, 1].TextString = data.TotalAreaBKFN.ToString("0.0");
-            table.Cells[8, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[3, 1].TextString = data.TotalAreaBKFN.ToString("0.0");
+            table.Cells[3, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            // Средняя этажность
+            table.Cells[4, 1].TextString = data.AverageFloors.ToString("0.0"); 
+            table.Cells[4, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            // Жителей
+            double population = data.TotalAreaApart * 0.05; // Всего площадь квартир/20
+            table.Cells[5, 1].TextString = population.ToString("0.0");
+            table.Cells[5, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             //ДОО, чел
-            table.Cells[9, 1].TextString = (data.TotalAreaApart* 0.00325).ToString("0.0"); //(("Всего площадь квартир"/20)/1 000)*65
-            table.Cells[9, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[6, 1].TextString = (data.TotalAreaApart* 0.00325).ToString("0.0"); //(("Всего площадь квартир"/20)/1 000)*65
+            table.Cells[6, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             //СОШ, чел
-            table.Cells[10, 1].TextString = (data.TotalAreaApart * 0.00675).ToString("0.0");//  (("Всего площадь квартир"/20)/1 000)*135
-            table.Cells[10, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[7, 1].TextString = (data.TotalAreaApart * 0.00675).ToString("0.0");//  (("Всего площадь квартир"/20)/1 000)*135
+            table.Cells[7, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             //Машиноместа, м/м
             var mm = data.TotalAreaApart * 0.021;
-            table.Cells[11, 1].TextString = mm.ToString("0.0");//  (("Всего площадь квартир"/20)/1 000)*420
-            table.Cells[11, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[8, 1].TextString = mm.ToString("0.0");//  (("Всего площадь квартир"/20)/1 000)*420
+            table.Cells[8, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
             //Машиноместа гостевые, м/м
-            table.Cells[12, 1].TextString = (mm * 0.25).ToString("0.0");//  Машиноместа %25
-            table.Cells[12, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
+            table.Cells[9, 1].TextString = (mm * 0.25).ToString("0.0");//  Машиноместа %25
+            table.Cells[9, 1].Borders.Bottom.LineWeight = LineWeight.LineWeight030;
 
             table.GenerateLayout();
             return table;
